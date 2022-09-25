3dac1109bc98111cd863faeefb1ab0820979706a
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-09-25 18:25:02 -0300

    Add: api e subindo o projeto

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,30 @@
 const Api = {
-// Ainda devo fazer o Deploy do Back-End
+    apiUrl: 'https://blueedtech-tarefas.herokuapp.com/tarefas',
+    fetchGetAll: () => fetch(`${Api.apiUrl}/get-tarefas`),
+    fetchGetById: id => fetch(`${Api.apiUrl}/get-tarefa-id/${id}`),
+    fetchPost: data => {
+        return fetch(`${Api.apiUrl}/create`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+        });
+    },
+    fetchPut: (tarefa, id) => {
+        return fetch(`${Api.apiUrl}/edit/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(tarefa),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+        });
+    },
+    fetchDelete: id => {
+        return fetch(`${Api.apiUrl}/delete/${id}`, {
+            method: 'DELETE',
+        });
+    },
+};
+
+export default Api;
